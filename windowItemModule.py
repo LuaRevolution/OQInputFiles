a3b82089b2da593bebdbd922d8b3421c3307150a
Ian <repicguy10@gmail.com>
2020-08-12 21:48:13 -0700

    wim complete

--- windowItemModule.py
+++ windowItemModule.py
@@ -2,14 +2,14 @@ import tkinter as tk
 import enum
 #classes
 class windowObject(enum.Enum):
-    ENTRY = 1
-    FULL_ENTRY = 2
-    DROPDOWN = 3
-    AUTO_ENTRY = 4
-    AUTO_FULL_ENTRY = 5
-    AUTO_DROPDOWN = 6
+    ENTRY = "ENTRY OBJECT"
+    FULL_ENTRY = "FULL_ENTRY OBJECT"
+    DROPDOWN = "DROPDOWN OBJECT"
+    FULL_DROPDOWN = "FULL_DROPDOWN OBJECT"
+    AUTOMATION = "AUTOMATION OBJECT"
 class Entry:
-    def __init__(self,master,type=None,label="Default",stringvar=None,defaultval=None):
+    def __init__(self,master,type=None,label="Default",stringvar=None,defaultval=None,pady=3):
+        # declarations
         if master == None:
             return 'No window'
         if type == None:
@@ -22,17 +22,18 @@ class Entry:
             self.stringvar = stringvar
         self.master = master
         self.label = label
+        # Gui
         self.Frame = tk.Frame(self.master)
         if self.type == windowObject.ENTRY:
             self.Label = tk.Label(self.Frame,text=self.label)
-            self.Label.pack(side=tk.LEFT)
+            self.Label.pack(side=tk.LEFT,pady=pady)
             self.Entry = tk.Entry(self.Frame,variable=stringvar)
-            self.Entry.pack(side=tk.LEFT)
+            self.Entry.pack(side=tk.LEFT,pady=pady)
         elif self.type == windowObject.FULL_ENTRY:
             self.Label = tk.Label(self.Frame,text=self.label)
-            self.Label.pack()
+            self.Label.pack(pady=pady)
             self.Entry = tk.Entry(self.Frame,variable=stringvar)
-            self.Entry.pack(fill=tk.BOTH,expand=True)
+            self.Entry.pack(fill=tk.BOTH,expand=True,pady=pady)
         self.Frame.pack()
     def __repr__(self):
         return self.get()
@@ -41,10 +42,12 @@ class Entry:
             if i is not None:
                 i.destroy()
         self.Frame.destroy()
+        del self
     def get(self):
         return self.Entry.get()
 class Dropdown:
-    def __init__(self,master,label="Default",stringvar=None,defaultval=None,options=None,noLabel=False,packType=None,command=None):
+    def __init__(self,master,label="Default",stringvar=None,defaultval=None,options=None,noLabel=None,packType=None,command=None,pady=3,type=windowObject.DROPDOWN):
+        # declarations
         if master == None:
             return 'No window'
         if stringvar == None:
@@ -59,84 +62,93 @@ class Dropdown:
             self.options = ["Default"]
             if self.stringvar is not None and defaultval is None:
                 self.stringvar.set(self.options[0])
+        if noLabel == None:
+            noLabel = False
+            self.noLabel = noLabel
+        else:
+            self.noLabel = noLabel
         self.master = master
         self.label = label
-        self.Frame = None
-        if noLabel == True:
-            self.Frame = self.master
-            self.OptionMenu = tk.OptionMenu(self.Frame,self.stringvar,*self.options,command=command)
-        else:
-            self.Frame = tk.Frame(self.master)
-            self.Label = tk.Label(self.Frame,text=self.label)
-            self.Label.pack(side=tk.LEFT)
-            self.OptionMenu = tk.OptionMenu(self.Frame,self.stringvar,*self.options,command=command)
-        self.OptionMenu.pack(side=packType)
+        self.Label = None
+
+        #gui
+        self.Frame = tk.Frame(self.master)
         self.Frame.pack()
+        self.OptionMenu = tk.OptionMenu(self.Frame,self.stringvar,*self.options,command=command)
+        if self.noLabel == False:
+            self.Label = tk.Label(self.Frame,text=self.label)
+            if type == windowObject.DROPDOWN:
+                self.Label.pack(side=self.packType,pady=pady)
+            elif type == windowObject.FULL_DROPDOWN:
+                self.Label.pack(pady=pady)
+            self.OptionMenu.pack(side=self.packType,pady=pady)
+        else:
+            self.OptionMenu.pack(side=self.packType,pady=pady)
     def __repr__(self):
         return self.get()
     def destroy(self):
-        if noLabel == False:
+        if self.noLabel == False:
             for k,i in self.Frame.children.copy().items():
                 if i is not None:
                     i.destroy()
             self.Frame.destroy()
-        elif noLabel == True:
+
+        elif self.noLabel == True:
             self.OptionMenu.destroy()
+        del self
     def get(self):
         return self.OptionMenu.get()
-class Automize:
-    def __init__(self,master,label="Default",type=None,stringvar=None,defaultval=None,checkButtonText=None,checkButtonCommand=None,checkButtonVar=None):
+class Automation:
+    def __init__(self,master,objectType,_checkbuttoncommand=None,_checkbuttontext=None,**kwargs):
         if master == None:
             return 'No window'
-        if checkButtonText == None:
-            self.checkButtonText = ("Automatically assign '{}'".format(label.replace(":","")))
-            print(self.checkButtonText)
-        else:
-            self.checkButtonText = checkButtonText
-        if checkButtonVar == None:
-            self.checkButtonVar = tk.IntVar()
-        else:
-            self.checkButtonVar = checkButtonVar
-        self.type = windowObject.AUTO_ENTRY
+        if _checkbuttontext == None:
+            try:
+                _checkbuttontext = ("Automatically assign '{}'".format(kwargs["label"].replace(":","")))
+            except:
+                _checkbuttontext = "No text"
+
+        self.type = windowObject.AUTOMATION
         self.master = master
-        self.entryObject = None
-        self.label = label
+        self.checkButtonVar = tk.IntVar()
         self.Frame = tk.Frame(self.master)
+        self.Frame.pack()
+        self.object = None
+
+        for i,v in kwargs.items():
+            print(i+" "+v)
 
         def checkButtonF():
             if self.checkButtonVar.get() == 0:
                 # uncheck
-                if self.entryObject is not None:
-                    self.entryObject.destroy()
-                if checkButtonCommand is not None:
-                    checkButtonCommand(checked=False)
+                if self.object is not None:
+                    self.object.destroy()
+                if _checkbuttoncommand is not None:
+                    _checkbuttoncommand(checked=False)
                 self.checkButtonVar.set(1)
             elif self.checkButtonVar.get() == 1:
                 # check
-                self.entryObject = Entry(self.Frame,label=self.label,type=windowObject.ENTRY,stringvar=stringvar,defaultval=defaultval)
-                if checkButtonCommand is not None:
-                    checkButtonCommand(checked=True)
+                if objectType == windowObject.ENTRY:
+                    self.object = Entry(self.Frame,**kwargs)
+                elif objectType == windowObject.DROPDOWN:
+                    self.object = Dropdown(self.Frame,**kwargs)
+                if _checkbuttoncommand is not None:
+                    _checkbuttoncommand(checked=True)
                 self.checkButtonVar.set(0)
-        self.checkButton = tk.Checkbutton(self.Frame,text=self.checkButtonText,command=checkButtonF)
+        self.checkButton = tk.Checkbutton(self.Frame,text=_checkbuttontext,command=checkButtonF)
         self.checkButton.pack()
         self.checkButton.deselect()
         self.checkButton.invoke()
-
-        self.Frame.pack()
     def __repr__(self):
-        return self.get()
+        return self.type
     def destroy(self):
-        for k,i in self.Frame.children.copy().items():
-            if i is not None:
-                i.destroy()
-        self.Frame.destroy()
+        self.object.destroy()
+        del self
     def get(self):
-        if self.entryObject is not None:
-            return self.Ent.get()
-        else:
-            return None
+        return self.object
 
 if __name__ == "__main__":
     root = tk.Tk()
-    test = AutoEntry(root,label="test")
+    test = Automation(root,windowObject.DROPDOWN,label="Test:")
+    test2 = Dropdown(root,noLabel=True)
     root.mainloop()
